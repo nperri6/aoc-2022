158fe9891020cc5476db46173f09e095bc4b9233
Nick Perri <nick.perri@lob.com>
2022-12-01 23:23:46 -0600

    working day 2

--- day2/part1.py
+++ day2/part1.py
@@ -1,3 +1,44 @@
+
+values = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3
+}
+
+def calcWin(plays):
+    if plays[0] == "A":
+        match plays[1]:
+            case "X":
+                return 3
+            case "Y":
+                return 6
+            case "Z":
+                return 0
+    elif plays[0] == "B":
+        match plays[1]:
+            case "X":
+                return 0
+            case "Y":
+                return 3
+            case "Z":
+                return 6
+    elif plays[0] == "C":
+        match plays[1]:
+            case "X":
+                return 6
+            case "Y":
+                return 0
+            case "Z":
+                return 3
+
+
+def score_line(line):
+    plays = line.split()
+    return values[plays[1]] + calcWin(plays)
+
 with open('input.txt') as f:
     lines = f.readlines()
- 
+    total = 0
+    for line in lines:
+        total += score_line(line)
+    print(str(total))
--- day2/part2.py
+++ day2/part2.py
@@ -1,2 +1,43 @@
+
+values = {
+    "A": 1,
+    "B": 2,
+    "C": 3
+}
+
+def calcMove(plays):
+    if plays[1] == "X":
+        match plays[0]:
+            case "A":
+                return 3
+            case "B":
+                return 1
+            case "C":
+                return 2
+    elif plays[1] == "Y":
+        match plays[0]:
+            case "A":
+                return 4
+            case "B":
+                return 5
+            case "C":
+                return 6
+    elif plays[1] == "Z":
+        match plays[0]:
+            case "A":
+                return 8
+            case "B":
+                return 9
+            case "C":
+                return 7
+
+def score_line(line):
+    plays = line.split()
+    return calcMove(plays)
+
 with open('input.txt') as f:
     lines = f.readlines()
+    total = 0
+    for line in lines:
+        total += score_line(line)
+    print(str(total))
