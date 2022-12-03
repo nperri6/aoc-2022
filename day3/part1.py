f78ef2dfa4d92ff018f7bdcee1677f8398c7d6b0
Nick Perri <nick.perri@lob.com>
2022-12-03 12:18:14 -0600

    add working day 3

--- day3/part1.py
+++ day3/part1.py
@@ -1,15 +1,25 @@
-map = {
-    "a":
-}
+
+from functools import reduce
+from math import floor
+
+def convertToNum(letter):
+    if (letter == letter.upper()):
+        return ord(letter) - 38
+    else:
+        return ord(letter) - 96
+
 
 with open('input.txt') as f:
     lines = f.readlines()
     dups = []
     for line in lines:
-        mid = len(line)/2 -1 
-        firstLetters = line[:mid].split("")
+        mid = floor(len(line)/2)
+        firstLetters = list(line[:mid])
         second = line [mid:]
         for letter in firstLetters:
             if letter in second:
                 dups.append(letter)
+                break
+    mappedValues = map(convertToNum, dups )
+    print(reduce(lambda a, b: a + b, mappedValues))
     
--- day3/part2.py
+++ day3/part2.py
@@ -1,2 +1,29 @@
+
+from functools import reduce
+
+def convertToNum(letter):
+    if (letter == letter.upper()):
+        return ord(letter) - 38
+    else:
+        return ord(letter) - 96
+
+def determineDup(currLines):
+    firstLetters = list(currLines[0])
+    for letter in firstLetters:
+        if letter in currLines[1] and letter in currLines[2]:
+            return letter
+
 with open('input.txt') as f:
     lines = f.readlines()
+    dups = []
+    currLines = []
+    for idx, line in enumerate(lines):
+        if idx > 0 and idx % 3 == 0:
+            dups.append(determineDup(currLines))
+            currLines = [line]
+        else:
+            currLines.append(line)
+    dups.append(determineDup(currLines))
+    mappedValues = map(convertToNum, dups )
+    print(reduce(lambda a, b: a + b, mappedValues))
+    
